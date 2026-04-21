012efdd29a6bf9ed0413bd69c14a1a0657731e07
hani kumar <132041059+hanikumar0@users.noreply.github.com>
2026-04-21 11:06:14 +0530

    Allow Vercel frontend in CORS policy

--- server/app.js
+++ server/app.js
@@ -21,8 +21,9 @@ const app = express();
 // Security Middlewares
 const allowedOrigins = [
   'http://localhost:3000',
-  process.env.FRONTEND_URL // We will set this in Render env vars
-];
+  'https://real-estate-crm-git-main-hani-kumars-projects.vercel.app',
+  process.env.FRONTEND_URL 
+].filter(Boolean);
 
 app.use(cors({
   origin: function(origin, callback) {
